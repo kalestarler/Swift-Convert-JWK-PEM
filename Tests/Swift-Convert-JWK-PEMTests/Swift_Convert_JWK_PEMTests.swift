84aadc20cbb3866e9188528c947a446af45f16e5
Kale <kale@kalestarler.com>
2023-11-12 22:00:52 +0800

    Initial Commit

---
+++ Tests/Swift-Convert-JWK-PEMTests/Swift_Convert_JWK_PEMTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Swift_Convert_JWK_PEM
+
+final class Swift_Convert_JWK_PEMTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
