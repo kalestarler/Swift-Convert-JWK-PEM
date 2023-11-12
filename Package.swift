40c934acc04ad45f67e4d3f759817e74f83af7d9
Kale <kale@kalestarler.com>
2023-11-12 23:11:06 +0800

    Removed unnecessary dependencies

--- Package.swift
+++ Package.swift
@@ -11,17 +11,11 @@ let package = Package(
             name: "Swift-Convert-JWK-PEM",
             targets: ["Swift-Convert-JWK-PEM"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/TakeScoop/SwiftyRSA", .upToNextMajor(from: "1.8.0")),
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Swift-Convert-JWK-PEM",
-            dependencies: [
-                .product(name: "SwiftyRSA", package: "SwiftyRSA")
-            ]),
+            name: "Swift-Convert-JWK-PEM"),
         .testTarget(
             name: "Swift-Convert-JWK-PEMTests",
             dependencies: ["Swift-Convert-JWK-PEM"]),
